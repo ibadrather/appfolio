17d8c5bd5db13ad39f85b982d19562a90c3981f7
Ibad Rather <ibad.rather.ir@gmail.com>
2023-04-13 04:08:36 +0200

    new port

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from projects import app as projects_app
+from portfolio import app as portfolio_app
+
+# Define the navigation menu
+PAGES = {
+    "Portfolio": portfolio_app,
+    "My Projects": projects_app,  # Add the My Projects page to the navigation menu
+}
+
+# Render the app
+def main():
+    # Set page title, icon, and layout width
+    st.set_page_config(
+        page_title="Ibad Rather",
+        page_icon=":robot_face:",
+        layout="wide",
+        initial_sidebar_state="collapsed",
+    )
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", list(PAGES.keys()))
+    PAGES[page]()
+
+if __name__ == "__main__":
+    main()
