8db5cd5aa98ecbb063270de15821bea88e0ad3db
Ibad Rather <ibad.rather.ir@gmail.com>
2023-05-06 13:53:40 +0200

    test try 2

--- Backend/get_objects_from_aws_s3_bucket.py
+++ Backend/get_objects_from_aws_s3_bucket.py
@@ -63,14 +63,14 @@ def get_image_from_s3_bucket(image_api_url: str) -> Image.Image:
 
 
 def lambda_api():
-    BASE_S3_LAMDA_API_GATEWAY_URL = os.environ.get("BASE_S3_LAMDA_API_GATEWAY_URL")
+    BASE_S3_LAMBDA_API_GATEWAY_URL = os.environ.get("BASE_S3_LAMBDA_API_GATEWAY_URL")
     BUCKET_NAME_REV_SEARCH_DATABASE_IMAGES = os.environ.get(
         "BUCKET_NAME_REV_SEARCH_DATABASE_IMAGES"
     )
 
     image_path = "images/000001_jpg.rf.5182495727dae412fdb258232cd8a86a.jpg"
     image_api_url = create_image_url_for_s3_lambda_api_gateway(
-        BASE_S3_LAMDA_API_GATEWAY_URL,
+        BASE_S3_LAMBDA_API_GATEWAY_URL,
         BUCKET_NAME_REV_SEARCH_DATABASE_IMAGES,
         image_path,
     )
--- tests/backend/test_s3api.py
+++ tests/backend/test_s3api.py
@@ -17,7 +17,7 @@ from Backend.get_objects_from_aws_s3_bucket import (
 def env_vars():
     load_dotenv()
     return {
-        "BASE_S3_LAMDA_API_GATEWAY_URL": os.environ["BASE_S3_LAMDA_API_GATEWAY_URL"],
+        "BASE_S3_LAMBDA_API_GATEWAY_URL": os.environ["BASE_S3_LAMBDA_API_GATEWAY_URL"],
         "BUCKET_NAME_REV_SEARCH_DATABASE_IMAGES": os.environ[
             "BUCKET_NAME_REV_SEARCH_DATABASE_IMAGES"
         ],
@@ -25,7 +25,7 @@ def env_vars():
 
 
 def test_lambda_api(env_vars) -> None:
-    base_url = env_vars["BASE_S3_LAMDA_API_GATEWAY_URL"]
+    base_url = env_vars["BASE_S3_LAMBDA_API_GATEWAY_URL"]
     bucket_name = env_vars["BUCKET_NAME_REV_SEARCH_DATABASE_IMAGES"]
 
     image_path = "images/000001_jpg.rf.5182495727dae412fdb258232cd8a86a.jpg"
